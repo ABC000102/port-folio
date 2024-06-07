2c6ffa49e1a7008be9b2d8c02226c052e9492433
ABC000102 <vishut432@gmail.com>
2024-06-07 11:31:39 +0530

    js code

---
+++ java-script/info-js.js
@@ -0,0 +1,12 @@
+const a=3;
+console.log(a);
+
+// a=10; // we cant reasign a constant variable. 
+// you must declare constant variable at the time of initialization.
+ 
+var b = 10;
+
+console.log(b);
+
+b=15;
+console.log(b);
